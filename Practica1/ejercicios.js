296bfcbfa2443474276c2d117b9c14d8309ed06f
joaco2752 <122044319@upq.edu.mx>
2025-05-19 17:26:45 -0600

    ejercicioC

--- Practica1/ejercicios.js
+++ Practica1/ejercicios.js
@@ -11,3 +11,6 @@ const cuadrado = (numero) => numero * numero;
 console.log(cuadrado(3))
 console.log(cuadrado(5))
 console.log(cuadrado(10))
+
+const miSaludo = (nombre,edad) => `Hola, me llamo ${nombre} y tengo ${edad} años`;
+console.log(miSaludo("Joaquin",22))
