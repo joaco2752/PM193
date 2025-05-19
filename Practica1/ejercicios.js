0fda93b2a27a177c60205411e4ec8f39a2b13ef7
joaco2752 <122044319@upq.edu.mx>
2025-05-19 17:12:53 -0600

    ejercicioB

--- Practica1/ejercicios.js
+++ Practica1/ejercicios.js
@@ -5,3 +5,9 @@ nombre = "Ana Maria";
 
 const saludo = `Hola,  ${nombre} .Tienes  ${edad}  años.`;
 console.log(saludo);
+
+const cuadrado = (numero) => numero * numero;
+
+console.log(cuadrado(3))
+console.log(cuadrado(5))
+console.log(cuadrado(10))
