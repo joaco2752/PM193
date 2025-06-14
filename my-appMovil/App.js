68a2253f001b45cdfaa6c0fac7c8056bb0ec6b0d
joaco2752 <122044319@upq.edu.mx>
2025-06-13 21:37:57 -0600

    zonas

--- my-appMovil/App.js
+++ my-appMovil/App.js
@@ -1,15 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
+/* ZONa1 Importaciones: */
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const Texto=(props)=>{
+  const{contenido}=props
+  return(
+    <Text>{contenido}</Text>
+  )
+}
+
+/* Zona2: Main */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Texto contenido="Hola"></Texto>
+      <Texto contenido="Mundo"></Texto>
+      <Texto contenido="ReactNative"></Texto>
+      <Button title="Presionar"></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+/* Zona3: Estilos */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
